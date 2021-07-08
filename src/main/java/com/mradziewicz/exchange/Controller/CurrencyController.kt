1053ecf949f915f2e63cd738fcfd168fa0f277c1
Radzi <radziewicz94@gmail.com>
2021-07-08 13:09:50 +0200

    zamiana z javy na kotlina, wydzielenie klasy odpowiedzialnej za obliczenia, poprawienie testów

--- src/main/java/com/mradziewicz/exchange/Controller/CurrencyController.kt
+++ src/main/java/com/mradziewicz/exchange/Controller/CurrencyController.kt
@@ -1,10 +1,7 @@
 package com.mradziewicz.exchange.Controller
 
-import com.mradziewicz.exchange.exception.NotFoundException
 import com.mradziewicz.exchange.service.ExchangeRateService
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
-import javax.xml.stream.events.Characters
 
 @RestController
 class CurrencyController(var exchangeRateService: ExchangeRateService) {
@@ -19,8 +16,8 @@ class CurrencyController(var exchangeRateService: ExchangeRateService) {
             return exchangeRateService.cantorExchangePlnToCurrency(curr!!, amount);
 
     }
-    @GetMapping("/exchangeTwoCurrency/{sellCurrency}/{buyCyrrency}/{amount}")
-    fun getSellExchangeTwoRate(@PathVariable sellCurrency: String, @PathVariable buyCyrrency: String, @PathVariable amount: Double): String{
-        return exchangeRateService.cantorExchangeTwoCurrency(sellCurrency, buyCyrrency, amount);
+    @GetMapping("/exchangeTwoCurrency/{sellCurrency}/{buyCurrency}/{amount}")
+    fun getSellExchangeTwoRate(@PathVariable sellCurrency: String, @PathVariable buyCurrency: String, @PathVariable amount: Double): String{
+        return exchangeRateService.cantorExchangeTwoCurrency(sellCurrency, buyCurrency, amount);
     }
 }
